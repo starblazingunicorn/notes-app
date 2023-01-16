c459d96ad28b6786edafe25ef91ddfedee728673
Viyan Abduljalel <viyan.abduljalel@arbetsformedlingen.se>
2023-01-16 08:53:01 +0100

    Modify save button

--- src/app/pages/note-card-details/note-card-details.component.ts
+++ src/app/pages/note-card-details/note-card-details.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Route, Router } from '@angular/router';
 import { Note } from 'src/app/shared/note.model';
 import { NotesService } from 'src/app/shared/notes.service';
 
@@ -11,12 +12,16 @@ import { NotesService } from 'src/app/shared/notes.service';
 export class NoteCardDetailsComponent {
   note: Note;
 
-  constructor(private notesService: NotesService) {}
+  constructor(private notesService: NotesService, private router:Router) {}
   ngOnInit() {
     this.note = new Note();
   }
   onSubmit(form: NgForm) {
     // console.log(form);
     this.notesService.add(form.value);
+    this.router.navigateByUrl('/');
+  }
+  cancel(){
+    this.router.navigateByUrl('/');
   }
 }
